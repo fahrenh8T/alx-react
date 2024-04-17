ea63c82a628cbf188b1475716d249ae6e70203b2
fahrenh8T <raymondrayn98@gmail.com>
2024-04-17 05:56:23 +0300

    conditionally applying style: task_2

---
+++ 0x02-react_props/task_0/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import holberton_logo from './holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={holberton_logo} className="App-logo" alt="logo" />
+          <h1>School dashboard</h1>
+        </header>
+      </div>
+
+      <div className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+      </div>
+
+      <div className="App-footer">
+        <p>
+          Copyright 2020 - holberton School
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
